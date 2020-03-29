8f924d6d2aeaf4c6aef773ff7e3971f225b7408f
AlexZhangrj <zhangrenjie1981@outlook.com>
2020-03-29 19:07:35 +0800

    防止apns被ddos掉

--- src/main/java/com/zhrenjie04/alex/util/AbstractRedisMap.java
+++ src/main/java/com/zhrenjie04/alex/util/AbstractRedisMap.java
@@ -1,14 +1,18 @@
 package com.zhrenjie04.alex.util;
 
 import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Type;
 
-public class RedisMap<V>{
+public abstract class AbstractRedisMap<V> {
 
 	protected Class<V> vClazz;
 
-	public RedisMap() {
-		vClazz = (Class<V>) ((ParameterizedType) getClass().getGenericSuperclass()).getActualTypeArguments()[0];
+	@SuppressWarnings("unchecked")
+	public AbstractRedisMap() {
+		Type superType=this.getClass().getGenericSuperclass();
+		vClazz = (Class<V>) ((ParameterizedType)superType).getActualTypeArguments()[0];//此方法必须写在父类中
 	}
+
 	public V get(String key) {
 		String json=RedisUtil.get(key);
 		return JsonUtil.parse(json, vClazz);
