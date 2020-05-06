7475452901a1acb34b7b177760cc944de0380e66
AlexZhangrj <zhangrenjie1981@outlook.com>
2020-05-06 21:16:55 +0800

    改进PageResult

--- src/main/java/com/zhrenjie04/alex/core/PageResult.java
+++ src/main/java/com/zhrenjie04/alex/core/PageResult.java
@@ -66,11 +66,20 @@ public class PageResult extends JsonResult{
 	public static PageResult success(){
 		return new PageResult();
 	}
-	public static PageResult success(String message,Long total,Object list, Long pageNo,Long pageSize,String orderBy,String orderType){
-		return new PageResult(CODE_SUCCESS,message,total,list,pageNo,pageSize,orderBy,orderType);
+	public static PageResult success(Long total,Object list, Long pageNo,Long pageSize,String orderBy,String orderType){
+		return new PageResult(CODE_SUCCESS,"",total,list,pageNo,pageSize,orderBy,orderType);
+	}
+	public static PageResult success(Long total,Object list, Long pageNo,Long pageSize){
+		return new PageResult(CODE_SUCCESS,total,list,pageNo,pageSize);
+	}
+	public static PageResult failure(Integer code,String message) {
+		return new PageResult(code,message);
 	}
 	public static PageResult failure(Integer code,String message,Long total,Object list, Long pageNo,Long pageSize,String orderBy,String orderType) {
-		return new PageResult(code,message,0L,list,pageNo,pageSize,orderBy,orderType);
+		return new PageResult(code,message,total,list,pageNo,pageSize,orderBy,orderType);
+	}
+	public static PageResult failure(Integer code,String message,Long total,Object list, Long pageNo,Long pageSize) {
+		return new PageResult(code,message,total,list,pageNo,pageSize);
 	}
 	public PageResult(Long total,Object list, Long pageNo,Long pageSize,String orderBy,String orderType){
 		put("total", total);
@@ -79,6 +88,41 @@ public class PageResult extends JsonResult{
 		put("pageSize", pageSize);
 		put("orderBy", orderBy);
 		put("orderType", orderType);
+		if (pageSize != null) {
+			put("totalPages", (total / pageSize) + (total % pageSize > 0 ? 1 : 0));
+		}
+	}
+	public PageResult(Long total,Object list, Long pageNo,Long pageSize){
+		put("total", total);
+		put("list", list);
+		put("pageNo", pageNo);
+		put("pageSize", pageSize);
+		if (pageSize != null) {
+			put("totalPages", (total / pageSize) + (total % pageSize > 0 ? 1 : 0));
+		}
+	}
+	public PageResult(Integer code,String message){
+		this();
+		super.setStatus(code);
+		super.setMessage(message);
+	}
+	public PageResult(Integer code,String message, Long total,Object list, Long pageNo,Long pageSize){
+		super.setStatus(code);
+		super.setMessage(message);
+		put("total", total);
+		put("list", list);
+		put("pageNo", pageNo);
+		put("pageSize", pageSize);
+		if (pageSize != null) {
+			put("totalPages", (total / pageSize) + (total % pageSize > 0 ? 1 : 0));
+		}
+	}
+	public PageResult(Integer code,Long total,Object list, Long pageNo,Long pageSize){
+		super.setStatus(code);
+		put("total", total);
+		put("list", list);
+		put("pageNo", pageNo);
+		put("pageSize", pageSize);
 		if (pageSize != null) {
 			put("totalPages", (total / pageSize) + (total % pageSize > 0 ? 1 : 0));
 		}
