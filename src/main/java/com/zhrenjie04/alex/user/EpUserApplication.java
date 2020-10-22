a1797006056541f9b23eb45decbd22b1a4d37f0e
张人杰 <zhangrenjie1981@126.com>
2020-10-22 22:43:46 +0800

    使用@ConfigurationProperties优化配置项

--- src/main/java/com/zhrenjie04/alex/user/EpUserApplication.java
+++ src/main/java/com/zhrenjie04/alex/user/EpUserApplication.java
@@ -8,7 +8,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
@@ -27,7 +26,14 @@ import org.springframework.web.servlet.mvc.method.RequestMappingInfo;
 import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerMapping;
 
 import com.zhrenjie04.alex.core.DynamicDataSourceConfig;
+import com.zhrenjie04.alex.core.EpAuthProp;
+import com.zhrenjie04.alex.core.EpHttpClientProp;
+import com.zhrenjie04.alex.core.EpIdGeneratorProp;
+import com.zhrenjie04.alex.core.EpImgProp;
+import com.zhrenjie04.alex.core.EpJwtProp;
 import com.zhrenjie04.alex.core.EpMultiDataSourceProp;
+import com.zhrenjie04.alex.core.EpRedisProp;
+import com.zhrenjie04.alex.core.EpSessionUtilProp;
 import com.zhrenjie04.alex.core.Permission;
 import com.zhrenjie04.alex.util.Base64ImageUtil;
 import com.zhrenjie04.alex.util.FileUploadUtil;
@@ -51,7 +57,10 @@ import springfox.documentation.oas.annotations.EnableOpenApi;
 //@EnableCaching
 @EnableWebMvc
 @EnableOpenApi
-@Import({ WebConfig.class, DynamicDataSourceConfig.class ,ContextConfig.class, EpMultiDataSourceProp.class })
+@Import({ WebConfig.class, DynamicDataSourceConfig.class ,ContextConfig.class, 
+	EpMultiDataSourceProp.class,EpRedisProp.class,EpHttpClientProp.class,
+	EpIdGeneratorProp.class, EpSessionUtilProp.class, EpImgProp.class, 
+	EpJwtProp.class, EpAuthProp.class })
 public class EpUserApplication implements ApplicationContextAware{
 
 	private static final Logger logger = LoggerFactory.getLogger(EpUserApplication.class);
@@ -65,121 +74,36 @@ public class EpUserApplication implements ApplicationContextAware{
 	public static void main(String[] args) {
 		SpringApplication.run(EpUserApplication.class, args);
 	}
-	
-	@Value("${id.worker-id}")
-	private Integer workerId = 0;
-	@Value("${img.urlPrefix}")
-	private String urlPrefix = "";
-	@Value("${img.nfs-folder}")
-	private String nfsFolder = "";
-	@Value("${jwt.jwt-auth-key}")
-	private String jwtAutheKey="";
-
-	@Value("${http.validate-inactivity-duration}")
-	private Integer validateInactivityDuration;
-	@Value("${http.wait-idle-connection-timeout}")
-	private Integer waitIdleConnectionTimeoutConfig;
-	@Value("${http.connect-timeout}")
-	private Integer connectTimeoutConfig;
-	@Value("${http.so-timeout-config}")
-	private Integer soTimeoutConfig;
-
-	/**
-	 * Redis服务器IP
-	 */
-	@Value("${redis.addr}")
-	private String addr = "127.0.0.1";
-
-	/**
-	 * Redis的端口号
-	 */
-	@Value("${redis.port}")
-	private int port = 6379;
-
-	/**
-	 * 访问密码
-	 */
-	@Value("${redis.auth}")
-	private String auth = "admin";
-
-	/**
-	 * 可用连接实例的最大数目，默认值为256；
-	 * 如果赋值为-1，则表示不限制；如果pool已经分配了maxActive个jedis实例，则此时pool的状态为exhausted(耗尽)。
-	 */
-	@Value("${redis.max-total}")
-	private int maxTotal = 256;
-
-	/**
-	 * 控制一个pool最多有多少个状态为idle(空闲的)的jedis实例，默认值也是8。
-	 */
-	@Value("${redis.max-idle}")
-	private int maxIdle = 200;
-
-	/**
-	 * 控制一个pool最少有多少个状态为idle(空闲的)的jedis实例，默认值也是8。
-	 */
-	@Value("${redis.min-idle}")
-	private int minIdle = 256;
-
-	/**
-	 * 等待可用连接的最大时间，单位毫秒，默认值为-1，表示永不超时。如果超过等待时间，则直接抛出JedisConnectionException；
-	 */
-	@Value("${redis.max-wait-millis}")
-	private long maxWaitMillis = 10000;
-
-	@Value("${redis.timeout}")
-	private int timeout = 10000;
-
-	@Value("${redis.database}")
-	private int database = 0;
-
-	@Value("${redis.global-keeptime}")
-	private int globalKeeptimeInput = 7200;
-	/**
-	 * 在borrow一个jedis实例时，是否提前进行validate操作；如果为true，则得到的jedis实例均是可用的；
-	 */
-	@Value("${redis.test-on-borrow}")
-	private boolean testOnBorrow = true;
-
-	@Value("${springCacheExpiration}")
-	private long springCacheExpiration;
-
-	@Value("${http.max-total}")
-	private Integer httpMaxTotal;
-	@Value("${http.max-per-route}")
-	private Integer maxPerRoute;
-	@Value("${http.so-timeout}")
-	private Integer soTimeout;
-	@Value("${http.retry-execution-count}")
-	private Integer retryExecutionCountConfig;
-	
-	@Value("${session.session-keep-time-seconds}")
-	private Integer sessionKeepTimeSecondsConfig = 20;
-
+	@Autowired
+	EpRedisProp redisProp;
+	@Autowired
+	EpHttpClientProp httpClientProp;
+	@Autowired
+	EpIdGeneratorProp idGeneratorProp;
+	@Autowired
+	EpSessionUtilProp sessionUtilProp;
+	@Autowired
+	EpImgProp imgProp;
+	@Autowired
+	EpJwtProp jwtProp;
+	@Autowired
+	EpAuthProp authProp;
 	@Autowired
 	private RequestMappingHandlerMapping requestMappingHandlerMapping;
 
 	@Bean
 	@Order(0)
-	public Object redisUtil() {
-		RedisUtil.init(maxTotal, maxIdle, minIdle, maxWaitMillis, testOnBorrow, globalKeeptimeInput, addr, port, timeout, auth, database);
-		HttpClientUtil.init(maxTotal, maxPerRoute, validateInactivityDuration, waitIdleConnectionTimeoutConfig, connectTimeoutConfig, soTimeoutConfig, retryExecutionCountConfig);
-		SessionUtil.init(sessionKeepTimeSecondsConfig);
-		FileUploadUtil.init(urlPrefix, workerId, nfsFolder);
-		Base64ImageUtil.init(urlPrefix, workerId, nfsFolder);
-		IdGenerator.init(workerId);
-		JwtUtil.init(jwtAutheKey);
+	public Object epUserServiceUtils() {
+		RedisUtil.init(redisProp.getMaxTotal(), redisProp.getMaxIdle(), redisProp.getMinIdle(), redisProp.getMaxWaitMillis(), redisProp.getTestOnBorrow(), redisProp.getGlobalKeeptime(), redisProp.getAddr(), redisProp.getPort(), redisProp.getTimeoutInMillis(), redisProp.getAuth(), redisProp.getDatabase());
+		HttpClientUtil.init(httpClientProp.getMaxTotal(), httpClientProp.getMaxPerRoute(), httpClientProp.getValidateInactivityDuration(), httpClientProp.getWaitIdleConnectionTimeout(), httpClientProp.getConnectTimeout(), httpClientProp.getSoTimeout(), httpClientProp.getRetryExecutionCount());
+		SessionUtil.init(sessionUtilProp.getSessionKeepTimeSeconds());
+		FileUploadUtil.init(imgProp.getUrlPrefix(), idGeneratorProp.getWorkerId(), imgProp.getNfsFolder());
+		Base64ImageUtil.init(imgProp.getUrlPrefix(), idGeneratorProp.getWorkerId(), imgProp.getNfsFolder());
+		IdGenerator.init(idGeneratorProp.getWorkerId());
+		JwtUtil.init(jwtProp.getJwtAuthKey());
 		return new Object();
 	}
 	
-//	@Bean
-//	public Object settingBean1(CacheManager cacheManager) {
-//		logger.info(">>>>>>>>>>" + cacheManager.getClass().getName());
-//		RedisCacheManager manager = (RedisCacheManager) cacheManager;
-//		manager.setDefaultExpiration(springCacheExpiration);
-//		return new Object();
-//	}
-
 	@Bean
 	public Object testBean(PlatformTransactionManager platformTransactionManager) {
 		System.out.println(">>>>>>>>>>" + platformTransactionManager.getClass().getName());
