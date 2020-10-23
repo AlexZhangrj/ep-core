61d6b7abac4f51551ec3e8db50022cf2d4dbdd1f
张人杰 <zhangrenjie1981@126.com>
2020-10-23 08:11:49 +0800

    暂存

--- src/main/java/com/zhrenjie04/alex/user/SwaggerConfig.java
+++ src/main/java/com/zhrenjie04/alex/user/SwaggerConfig.java
@@ -1,13 +1,19 @@
 package com.zhrenjie04.alex.user;
 
+import java.lang.reflect.Field;
 import java.util.Collections;
 import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Set;
 
+import org.apache.commons.lang3.reflect.FieldUtils;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.util.ReflectionUtils;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import io.swagger.v3.oas.models.security.SecurityScheme.In;
 import springfox.documentation.builders.ApiInfoBuilder;
@@ -26,7 +32,7 @@ import springfox.documentation.spring.web.plugins.Docket;
 
 @EnableOpenApi
 @Configuration
-public class SwaggerConfig {
+public class SwaggerConfig implements WebMvcConfigurer{
 	@Bean
     public Docket createRestApi() {
 		List<SecurityScheme> securitySchemes=new LinkedList<>();
@@ -48,6 +54,7 @@ public class SwaggerConfig {
 		);
                         
         return new Docket(DocumentationType.OAS_30).pathMapping("/")
+        		.enable(true)
         		.apiInfo(apiInfo())
                 .select()
                 .apis(RequestHandlerSelectors.any())
@@ -56,8 +63,9 @@ public class SwaggerConfig {
                 // 支持的通讯协议集合
                 .protocols(protocols)
                 // 授权信息设置，必要的header token等认证信息
-                .securitySchemes(securitySchemes)
-                .securityContexts(securityContexts);
+                .securitySchemes(securitySchemes);
+//                // 授权信息全局应用
+//                .securityContexts(securityContexts);
     }
 
     /**
@@ -70,4 +78,27 @@ public class SwaggerConfig {
                 .version("2.0.4")
                 .build();
     }
+    
+//    /**
+//     * 通用拦截器排除swagger设置，所有拦截器都会自动加swagger相关的资源排除信息
+//     */
+//    @SuppressWarnings("unchecked")
+//    @Override
+//    public void addInterceptors(InterceptorRegistry registry) {
+//        try {
+//            Field registrationsField = FieldUtils.getField(InterceptorRegistry.class, "registrations", true);
+//            List<InterceptorRegistration> registrations = (List<InterceptorRegistration>) ReflectionUtils.getField(registrationsField, registry);
+//            if (registrations != null) {
+//                for (InterceptorRegistration interceptorRegistration : registrations) {
+//                    interceptorRegistration
+//                            .excludePathPatterns("/swagger**/**")
+//                            .excludePathPatterns("/webjars/**")
+//                            .excludePathPatterns("/v3/**")
+//                            .excludePathPatterns("/doc.html");
+//                }
+//            }
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+//    }
 }
