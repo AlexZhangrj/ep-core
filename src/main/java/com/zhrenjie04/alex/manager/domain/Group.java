18f6acf8a0d54efd76a953e46157747025aad6f3
张人杰 <zhangrenjie1981@126.com>
2020-10-24 15:02:32 +0800

    暂存

---
+++ src/main/java/com/zhrenjie04/alex/manager/domain/Group.java
@@ -0,0 +1,26 @@
+package com.zhrenjie04.alex.manager.domain;
+
+import java.util.Date;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
+
+@Data
+@EqualsAndHashCode(of = {"groupId"}, callSuper = false)
+@ToString
+public class Group {
+	private String groupId;
+	private String groupName;
+	private String groupCode;
+	private String parentId;
+	private String idPath;
+	private Boolean isLocked=false;
+	private Boolean isDeleted=false;
+	private Date createdTime;
+	private String createrId;
+	private String createrName;
+	private Date lastModifiedTime;
+	private String lastModifierId;
+	private String lastModifierName;
+}
