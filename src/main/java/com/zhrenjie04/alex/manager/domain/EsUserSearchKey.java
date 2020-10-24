ca3e8785744d0de06480e4f4ae9f30f1c603f5d1
张人杰 <zhangrenjie1981@126.com>
2020-10-24 11:53:20 +0800

    完成ES数据的保存

---
+++ src/main/java/com/zhrenjie04/alex/manager/domain/EsUserSearchKey.java
@@ -0,0 +1,23 @@
+package com.zhrenjie04.alex.manager.domain;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.elasticsearch.annotations.Document;
+import org.springframework.data.elasticsearch.annotations.Field;
+import org.springframework.data.elasticsearch.annotations.FieldType;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
+
+@Document(indexName = "es_user_search_key",shards = 10, replicas =2)
+@Data
+@ToString
+@EqualsAndHashCode(of = {"userId"})
+public class EsUserSearchKey {
+	@Id
+	private String userId;
+	@Field(type = FieldType.Keyword)
+	private String username;
+	@Field(type = FieldType.Keyword)
+	private String cellphone;
+}
