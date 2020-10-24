4ce21213ff6cc6a0cf4fa6f05d0b101fbe4e9d76
张人杰 <zhangrenjie1981@126.com>
2020-10-24 16:18:21 +0800

    优化代码

--- src/main/java/com/zhrenjie04/alex/core/interceptor/AuthorizationInterceptor.java
+++ src/main/java/com/zhrenjie04/alex/core/interceptor/AuthorizationInterceptor.java
@@ -12,7 +12,6 @@ import javax.servlet.http.HttpServletResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.method.HandlerMethod;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
