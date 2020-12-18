d76e182636a24f675b5bd38ba815bf83a8885ef4
张人杰 <zhangrenjie1981@126.com>
2020-12-18 21:19:46 +0800

    提取抽象Aspect类和方法

---
+++ src/main/java/com/zhrenjie04/alex/core/AlexOperationLogAspect.java
@@ -0,0 +1,68 @@
+package com.zhrenjie04.alex.core;
+
+import com.zhrenjie04.alex.util.JsonUtil;
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.AfterThrowing;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.LinkedList;
+import java.util.List;
+
+@Slf4j
+public abstract class AlexOperationLogAspect {
+
+	public void  doBefor(JoinPoint joinPoint){
+		OperationLog operationLog = new OperationLog();
+		List<Object> args=new LinkedList<>();
+		for(Object arg:joinPoint.getArgs()){
+			if(arg instanceof HttpServletRequest){
+				continue;
+			}
+			if(arg instanceof HttpServletResponse){
+				continue;
+			}
+			args.add(arg);
+		}
+		operationLog.setParamsJson(JsonUtil.newSerializer().without(User.class,"password,oldPassword").doStringify(args));
+		log.info("Operation Start:{}",operationLog);
+	}
+	public void  doAfterReturning(JoinPoint joinPoint,Object result){
+		OperationLog operationLog = new OperationLog();
+		List<Object> args=new LinkedList<>();
+		for(Object arg:joinPoint.getArgs()){
+			if(arg instanceof HttpServletRequest){
+				continue;
+			}
+			if(arg instanceof HttpServletResponse){
+				continue;
+			}
+			args.add(arg);
+		}
+		operationLog.setParamsJson(JsonUtil.newSerializer().without(User.class,"password,oldPassword").doStringify(args));
+		operationLog.setResultJson(JsonUtil.stringify(result));
+		log.info("Operation Finished:{}",operationLog);
+    }
+	public void  doAfterThrowing(JoinPoint joinPoint,Exception e){
+		OperationLog operationLog = new OperationLog();
+		List<Object> args=new LinkedList<>();
+		for(Object arg:joinPoint.getArgs()){
+			if(arg instanceof HttpServletRequest){
+				continue;
+			}
+			if(arg instanceof HttpServletResponse){
+				continue;
+			}
+			args.add(arg);
+		}
+		operationLog.setParamsJson(JsonUtil.newSerializer().without(User.class,"password,oldPassword").doStringify(args));
+		operationLog.setExceptionJson(JsonUtil.stringify(e));
+		log.info("Operation Exception:{}",operationLog);
+	}
+}
